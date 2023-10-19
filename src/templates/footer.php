8f118f279e5460bba99366bcc78f46cb94afc845
olegeliseev <olegeliseev96@gmail.com>
2023-10-19 22:56:23 +0300

    added github link in footer

--- src/templates/footer.php
+++ src/templates/footer.php
@@ -1,3 +1,8 @@
+<div class="credits">
+    <span>Made by <a href="https://github.com/olegeliseev/Students/">olegeliseev</a></span>
+    <i class="fa-brands fa-github"></i>
+</div>
 </main>
+<script src="https://kit.fontawesome.com/ac6250e557.js" crossorigin="anonymous"></script>
 </body>
 </html>
